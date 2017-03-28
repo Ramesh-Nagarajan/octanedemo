34852b6797187bd257cd5ed9b2c1c1cfdd98202a
yuval <yuval.koren@hpe.com>
2017-03-28 16:11:41 +0300

    Commit all changes to the testing environment

---
+++ src/test/java/feature/manualRunner/JunitTestSuite.java
@@ -0,0 +1,17 @@
+package feature.manualRunner;
+
+/**
+ * Created by alm_alm_auto on 2/5/2017.
+ */
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        RegularJunitTests.class
+})
+
+public class JunitTestSuite {
+}
---
+++ src/test/java/feature/manualRunner/RegularJunitTests.java
@@ -0,0 +1,67 @@
+package feature.manualRunner;
+/**
+ * Created by alm_alm_auto on 2/17/2016.
+ */
+
+import junit.framework.TestCase;
+import org.junit.*;
+
+
+public class RegularJunitTests {
+
+    @Test
+    public void thisIsMyPassedTest8() {
+
+    }
+
+
+    @Test
+    public void thisIsMyPassedTest7() {
+
+    }
+
+
+    @Test
+    public void thisIsMyPassedTest6() {
+
+    }
+
+
+    @Test
+    public void thisIsMyPassedTest5() {
+
+    }
+
+    @Test
+    public void thisIsMyPassedTest4() {
+
+    }
+
+
+    @Test
+    public void thisIsMyPassedTest3() throws Exception{
+        throw new Exception("failing this test on purpose to show a failed te st");
+
+    }
+
+
+    @Test
+    public void thisIsMyPassedTest2() throws Exception{
+        throw new Exception("failing this test on purpose to show a failed te st");
+
+    }
+
+
+    @Test
+    public void thisIsMyPassedTest1() throws Exception{
+        throw new Exception("failing this test on purpose to show a failed te st");
+
+    }
+
+
+    @Test
+    public void thisIsMyFailedTest() throws Exception {
+        throw new Exception("failing this test on purpose to show a failed te st");
+    }
+
+}
